bd36dc358b58daf3cd42ff098d69514f9d4f9b6e
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-14 16:28:06 +0200

    practicle-14-01-21

--- src/index.js
+++ src/index.js
@@ -5,12 +5,36 @@ import movieItem from './templates/movieItem.hbs';
 
 const input = document.querySelector('.input');
 const list = document.querySelector('.list');
+const loadBtn = document.querySelector('.load');
+console.log(loadBtn);
+let page = 1;
+let inputValue = '';
 
 input.addEventListener('input', debounce(getMovie, 500));
+loadBtn.addEventListener('click', loadMore);
 
 function getMovie() {
-  const inputValue = input.value;
-  fetchMovie(inputValue).then(data => createMarkup(data.results));
+  inputValue = input.value;
+  list.innerHTML = '';
+  loadBtn.style.display = 'none';
+  page = 1;
+
+  if (inputValue.trim() === '') return;
+
+  fetchMovie(inputValue)
+    .then(data => {
+      createMarkup(data.results);
+      return data;
+    })
+    .then(data => {
+      console.log(2222);
+      if (!data.results.length) {
+        console.log('1111');
+        loadBtn.style.display = 'none';
+        return;
+      }
+      loadBtn.style.display = 'inline-block';
+    });
 }
 
 function createMarkup(array) {
@@ -21,4 +45,9 @@ function createMarkup(array) {
 
   const markup = movieItem(array);
   list.insertAdjacentHTML('beforeend', markup);
+}
+
+function loadMore() {
+  page += 1;
+  fetchMovie(inputValue, page).then(data => createMarkup(data.results));
 }
--- src/movieService.js
+++ src/movieService.js
@@ -1,8 +1,15 @@
-export default function fetchMovie(query) {
+export default function fetchMovie(query, page = 1) {
   const KEY = '3a77cf2e264fc5181ae75199083953b5';
   const BASE_URL = 'https://api.themoviedb.org/3/search/movie?';
 
   return fetch(
-    `${BASE_URL}api_key=${KEY}&language=en-US&page=1&include_adult=false&query=${query}`,
-  ).then(response => response.json());
+    `${BASE_URL}api_key=${KEY}&language=en-US&page=1&include_adult=false&query=${query}&page=${page}`,
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => console.log(error));
 }
